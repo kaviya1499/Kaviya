7243f14fb681adef172744554b58301cd1c5ecd6
kaviya1499 <kavikasi99@gmail.com>
2023-06-12 18:13:55 +0530

    project structure changed

---
+++ src/main/java/com/atdxt/JdbcConnection/Service/UserService.java
@@ -0,0 +1,26 @@
+package com.atdxt.JdbcConnection.Service;
+
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import com.atdxt.JdbcConnection.Model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+
+
+
+public interface UserService {
+
+    List <User> getAllUsers();
+
+    User createUser(User user);
+
+
+
+}
